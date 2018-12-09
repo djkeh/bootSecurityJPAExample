06efd8a8a4ac348f19678ac3af4b9d341f79302e
Uno Kim <djkehh@gmail.com>
2018-12-10 05:11:09 +0900

    Add #1 - JUnit 5
    This adds JUnit 5 dependency and applies it to original default test code.

--- src/test/java/com/me/sample/SampleApplicationTests.java
+++ src/test/java/com/me/sample/SampleApplicationTests.java
@@ -1,15 +1,18 @@
 package com.me.sample;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SampleApplicationTests {
 
     @Test
+    @DisplayName("Spring Boot 2 default context loading test")
     public void contextLoads() {
     }
 
