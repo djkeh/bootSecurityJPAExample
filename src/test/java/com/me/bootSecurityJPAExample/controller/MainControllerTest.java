47c7876f98b43778ff266e7a3c91b2a65aaf16b4
Uno Kim <djkehh@gmail.com>
2018-12-12 01:36:19 +0900

    Refactor #1 - gradle script & MainControllerTest
    I eventually failed to find the answer to solve
    travis-ci warning: annotationProcessor problem.
    This commit removes unused plugins
    and combines older gradle syntax
    to run gradle tasks without trouble.
    
    This also adds basic `MainController` test code.

---
+++ src/test/java/com/me/bootSecurityJPAExample/controller/MainControllerTest.java
@@ -0,0 +1,47 @@
+package com.me.bootSecurityJPAExample.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+// import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Main controller tests")
+class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("[GET] main view")
+    void main() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+
+    }
+}
