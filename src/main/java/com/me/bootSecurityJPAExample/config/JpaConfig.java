e42d93fe6a8083a45cdbaa2336fe3de655271e69
Uno Kim <djkehh@gmail.com>
2018-12-13 03:08:05 +0900

    Update #1 security, jpa config

--- src/main/java/com/me/bootSecurityJPAExample/config/JpaConfig.java
+++ src/main/java/com/me/bootSecurityJPAExample/config/JpaConfig.java
@@ -1,9 +1,15 @@
 package com.me.bootSecurityJPAExample.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Optional;
 
 
 @EnableTransactionManagement
@@ -11,5 +17,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            return Optional.ofNullable(auth == null ? null : auth.getName());
+        };
+    }
 
 }
--- src/main/java/com/me/bootSecurityJPAExample/config/SecurityConfig.java
+++ src/main/java/com/me/bootSecurityJPAExample/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package com.me.bootSecurityJPAExample.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // H2-console security config
         http.authorizeRequests()
-                .mvcMatchers("/h2-console/**").permitAll()
-            .and()
-                .headers().frameOptions().disable()
-            .and()
-                .csrf().disable();
+                .antMatchers("/h2-console/**", "/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .usernameParameter("loginId")
+                .passwordParameter("password")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll()
+                .logoutSuccessUrl("/")
+                .and()
+            .headers()
+                .frameOptions().disable()
+                .and()
+            .csrf()
+                .disable();
     }
 }
