6d19472336f8d9b0bcff5a4896d54000c1d41152
Uno Kim <djkehh@gmail.com>
2018-12-14 15:05:35 +0900

    Refactor #3 - MainControllerTest
    This commit tries refactoring of `MainCongtrollerTest`, to have `MockMvc` work as a unit test entry point, not as a integration test entry.
    
    The content string assertions in each test are removed, because they're not focusing on the controller, so they belong to the view or integration test.

--- src/test/java/com/me/bootSecurityJPAExample/controller/MainControllerTest.java
+++ src/test/java/com/me/bootSecurityJPAExample/controller/MainControllerTest.java
@@ -10,21 +10,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.Filter;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -36,7 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("Main controller tests")
 class MainControllerTest {
 
-    @Autowired private WebApplicationContext context;
+    @Autowired private MainController mainController;
+    @Autowired private Filter springSecurityFilterChain;
     @Autowired private ObjectMapper mapper;
     @MockBean private UserService userService; // TODO: @MockBean marks the cache of Spring context dirty, thereby slows down overall tests.
 
@@ -45,13 +43,12 @@ class MainControllerTest {
     @BeforeEach
     void before() {
         mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
+                .standaloneSetup(mainController)
+                .addFilter(springSecurityFilterChain)
                 .build();
     }
 
     @Test
-    @WithMockUser("test")
     @DisplayName("[GET] main view")
     void main() throws Exception {
         // Given
@@ -62,10 +59,12 @@ class MainControllerTest {
         given(userService.getUser(any())).willReturn(user);
 
         // When & Then
-        mvc.perform(get("/"))
+        mvc.perform(
+                get("/")
+                        .with(user("user"))
+        )
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Java + Spring Boot 2 + Security + JPA Example")))
-                .andExpect(content().string(containsString("사용자 리스트")))
+                .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("users"))
                 .andExpect(model().attributeExists("myAccount"));
         verify(userService).getList();
@@ -73,7 +72,6 @@ class MainControllerTest {
     }
 
     @Test
-    @WithMockUser("test")
     @DisplayName("[POST] add a user")
     void formMain() throws Exception {
         // Given
@@ -87,10 +85,13 @@ class MainControllerTest {
 
 
         // When & Then
-        mvc.perform(post("/").contentType(MediaType.APPLICATION_JSON).content(body))
+        mvc.perform(
+                post("/")
+                        .with(user("user"))
+                        .content(body)
+        )
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Java + Spring Boot 2 + Security + JPA Example")))
-                .andExpect(content().string(containsString("사용자 리스트")))
+                .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("users"))
                 .andExpect(model().attributeExists("myAccount"));
         verify(userService).getList();
