a094d8bfa5c5ef8767991f168d15bbcc95355dda
Uno Kim <djkehh@gmail.com>
2018-12-18 19:59:36 +0900

    Apply #3 - nested tests on SpringDataRestAPITests
    This commit applies nested tests on `SpringDataRestAPITests` to categorize unit tests. More unit tests are created as well.

--- src/test/java/com/me/bootSecurityJPAExample/SpringDataRestAPITests.java
+++ src/test/java/com/me/bootSecurityJPAExample/SpringDataRestAPITests.java
@@ -2,9 +2,8 @@ package com.me.bootSecurityJPAExample;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.me.bootSecurityJPAExample.domain.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.me.bootSecurityJPAExample.repository.UserRepository;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,9 +14,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -28,52 +30,119 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SpringDataRestAPITests {
 
     @Autowired private WebApplicationContext context;
+    @Autowired private UserRepository userRepository;
     @Autowired private ObjectMapper mapper;
 
     private MockMvc mvc;
+    private String baseUrl;
+    private String searchUrl;
 
     @BeforeEach
     void before() {
+        baseUrl = "/api/users";
+        searchUrl = baseUrl + "/search";
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
     }
 
-    @Test
-    @DisplayName("[POST] Create a user")
-    void createUserTest() throws Exception {
-        // Given
-        User user = User.builder()
-                .loginId("test")
-                .password("password")
+    @AfterEach
+    void after() {
+        userRepository.deleteAll(); // TODO: failed to apply rollback transactions among Spring Data Rest test cases.
+    }
+
+
+    @Nested
+    @DisplayName("for basic CRUD APIs")
+    class ForBasicAPIs {
+        @Test
+        @DisplayName("[POST] Create a user")
+        void createUserTest() throws Exception {
+            // Given
+            User user = constructTestUser("newUser", "pw");
+            String body = mapper.writeValueAsString(user);
+
+            // When & Then
+            mvc.perform(
+                    post(baseUrl)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(body)
+            )
+                    .andExpect(status().isCreated());
+        }
+
+        @Test
+        @DisplayName("[GET] Get a user info.")
+        void showUserTest() throws Exception {
+            // Given
+            User savedUser = userRepository.save(constructTestUser("test", "pw"));
+            long id = savedUser.getId();
+
+            // When & Then
+            mvc.perform(get(baseUrl + "/" + id))
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("[GET] Get an entire user list.")
+        void showUsersTest() throws Exception {
+            // Given
+
+            // When & Then
+            mvc.perform(get(baseUrl))
+                    .andExpect(status().isOk());
+        }
+    }
+
+
+    @Nested
+    @DisplayName("for user-custom(search) APIs")
+    class ForSearchAPIs {
+        @Test
+        @DisplayName("[GET] Search a user with login_id")
+        void showUsersTest() throws Exception {
+            // Given
+            String loginId = "testId";
+            String password = "testPassword";
+            User user = constructTestUser(loginId, password);
+            userRepository.save(user);
+
+            // When & Then
+            mvc.perform(
+                    get(searchUrl + "/findByLoginId")
+                            .param("loginId", loginId)
+            )
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(containsString(password)));
+        }
+
+        @Test
+        @DisplayName("[GET] Check if a user with login_id exists")
+        void checkIfExists() throws Exception {
+            // Given
+            String loginId = "testId";
+            User user = constructTestUser(loginId, "pw");
+            userRepository.save(user);
+
+            // When & Then
+            mvc.perform(
+                    get(searchUrl + "/existsByLoginId")
+                            .param("loginId", loginId)
+            )
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(is(Boolean.TRUE.toString())));
+        }
+    }
+
+
+    // Stubs
+    private User constructTestUser(String loginId, String password) {
+        return User.builder()
+                .loginId(loginId)
+                .password(password)
                 .createdBy("creator")
                 .updatedBy("creator")
                 .build();
-        String body = mapper.writeValueAsString(user);
-
-        // When & Then
-        mvc.perform(post("/api/users").contentType(MediaType.APPLICATION_JSON).content(body))
-                .andExpect(status().isCreated());
-    }
-
-    @Test
-    @DisplayName("[GET] Get a user info.")
-    void showUserTest() throws Exception {
-        // Given
-
-        // When & Then
-        mvc.perform(get("/api/users/1"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("[GET] Get an entire user list.")
-    void showUsersTest() throws Exception {
-        // Given
-
-        // When & Then
-        mvc.perform(get("/api/users"))
-                .andExpect(status().isOk());
     }
 }
